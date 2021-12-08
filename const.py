b6a4aa341f008ee4e5bc46bcd071f115ab09fac5
SergioDev22 <louis.sergio.rakotoarisoa@esti.mg>
2021-12-08 05:38:05 -0600

    Fichier des textes constantes

---
+++ const.py
@@ -0,0 +1,141 @@
+pageInfo = "Les informations concernants notre page arrivent bientôt ici"
+
+invalideFormatDate = "Votre date est invalide\n\nVeuillez saisir à nouveau \
+et suivez le bon format😊😊😊:"
+
+invalidLastDate = "Votre date est invalide\n\nPeut être cette date est déjà passée\n\n \
+Alors veuillez-vous saisir à nouveau et faire le bon choix pour la future date et pourqoui \
+ pas maintenant 😊😊😊"
+
+produitDispo = "Voici donc les differents terrains disponibles"
+
+noExistingDate = "Pour cette date, il n'y a pas encore des reservations, \
+donc vous êtes libre de choisir vos heures entre 6h00 à 20h00"
+
+ivalideHourFormat = "Votre heure est invalide\nVeuillez saisir à nouveau \
+et suivez le bon format\nMerci😊😊😊"
+
+ErrorFirstInterval = "Votre heure de DEBUT est tombé \
+dans l'intervalle de temps des heures déjà réservés\n\n Donc, Veuillez-vous saisir \
+à nouveau et bien verifier votre heure\n\n Merci 😊😊😊"
+
+ErrorSecondIntervall = "Votre heure de DEBUT est tombé dans \
+l'intervalle de temps des heures déjà réservés\n\nDonc, \
+Veuillez-vous saisir à nouveau et bien verifier votre heure\n\n Merci 😊😊😊"
+
+ErrorThirdInterval = "Votre heure de FIN est tombé dans \
+l'intervalle de temps des heures déjà réservés\n\nDonc, Veuillez-vous saisir à \
+nouveau et bien verifier votre heure\n\n Merci 😊😊😊"
+
+inputBeginingHour = "Saisir alors votre heure de Debut en format HHhMM\nExemple: 12h00 ou 07h30"
+inputFinalHour = "Saisir votre heure de fin alors en format HHhMM\n Exemple 12h00 ou 09h00"
+
+inputNewBeginingHour = "Saisir alors votre nouveau heure de Debut en format HHhMM\nExemple: 12h00 ou 07h30"
+inputNewFinalHour = "Veuillez saisir à nouveau donc:"
+
+ErrorTranceBegining = "Votre Heure de DEBUT est invalide car il ne respecte pas le marge de \
+la tranche\n\nVeuillez saisir à nouveau en respectant la marge de trance\n\nMerci😊😊😊"
+
+ErrorMarging = "Votre heure de Fin est ivalide car selon la marge du commande \
+, il est forcement plus d'une heure le commande\n\n Alors \
+veuillez-vous saisir à nouveau en respectant cette marge\n\nMerci😊😊😊"
+
+ErrorTranceEnd = "Votre Heure de FIN est invalide car il ne respecte pas le marge de \
+la tranche\n\nVeuillez saisir à nouveau en respectant la marge de trance\n\nMerci😊😊😊"
+
+givingTicket = "Voici donc votre QRCODE qui est votre ticket d'entrée le jour où \
+vous serez au terrain\n\n Alors le gardé bien 😊😊😊"
+
+roulesOfHour = "Maintenat, je vous informe pour les reglements de l'heure que vous \
+entrez. Tout d'abord, le minimum du temps du commande est d'une heure(1h). Ensuite l'intervalle \
+du temps du commande est par la trance de 30 minutes ou 1h, c-a-d votre commande est alors \
+1h ou 1h30 ou 2h du temps ou ainsi de suite. Et enfin votre heure de fin et heure de début sont alors exprimées \
+de façon 13h30 pour le debut par example et 15h00 celle de la fin, c-a-d il n'y a que de 30 ou 00 le \
+minute des heures que vous entrez.\n\n Pour terminer, regardez bien les intervalles du temps déjà reservés \
+pour votre bonne choix et surtout le format de l'heure qu'on vous suposse à entrer\n\nMerci 😊😊😊"
+
+cmdOfAnotherDate = "Entrer alors la date en respctant toujours le bon format😊😊😊"
+
+curiosity = "Merci beaucoup pour votre curiosité et la visite de notre page😊😊😊\n\n \
+Vous pouvez encore  faire un commade maintenant même ou à une autre jour si vous voulez en envoyant \
+encore de message 😉😉😉\n\nSinon A Bientôt ✋✋✋✋✋"
+
+informations = "Votre commande a été bien enregistrer\n\nPour que nous pouvons \
+confirmer vraiment votre commande, on vous demande de payer une pétite avance du montant \
+5000Ar et le reste de paymet aura lieu le jour où vous serez au terrain\n\n \
+Voici donc notre numero:\nTELMA:0340000000(Nom:Paul jean BA)\n \
+ORANGE:032000000(Nom:rakoto bleu)\n\nEt après saisir ici votre reference de transaction(celle-ci \
+est comme l'image ci-dessous montre!!\nAlors si vous avez payé l'avance auprès du \
+CACHE POINT MOBILE MONEY, n'oublie pas de la recuperer chez eux\n\n \
+Alors, on vous attend pour l'envoi de l'avance et la reférence ici"
+
+problems = "Et si vous avez de probleme pour l'envoi de cet avance, vous pouvez appelez \
+les numéro 034000000 et 032000000\n\n ON vous attend donc pour la saisie de la reference"
+
+inputReference = "Saisir alors votre REFERENCE: "
+
+thanking = "Merci pour la discussion......"
+
+receivedHourBegining = "Votre heure de debut est bien reçu!!!\n\nVous pouvez annuler au cas où  \
+vous en avez besoin et continuer si c'est pas le cas!!!"
+
+Error30Marge = "Votre heure de début n'est pas mal mais selon notre marge(1h minimum du commande) \
+votre choix d'heure de début est alors invalide parce que ca va risqué toujours tombé dans les \
+intervalles des temps qui existent votre heure de fin\n\n \
+Donc on vous suppose de re-ecriver votre heure de debut et choisir la bonne en respectant toujour \
+le format et d'eviter aussi l'erreur des intervalles de temps\n\nMerci 😊😊😊" 
+
+inputUserNameOtherUser = "Entrer alors votre nom d'utilisateur:"
+inputPassWordOtherUser = "Entrer votre mot de passe: "
+
+salutationAdmin = "Salut Admin,Ravi de vous acceuillir 😊😊😊"
+
+ErrorFormatUserMail = "Votre nom d'utilisateur est invalide\nVeuillez verifier son format\nMerci😊😊😊"
+ErrorLoginAdmin = "Votre nom d'utilasateur et mot de passe sont incorrects\n\n \
+Veuillez-vous saisir à noveau!!!"
+
+thankingInfo = "Merci pour la visite chez nous 😊😊😊"
+
+modifSuccess = "Modifié avec SUCCÉS"
+
+ErrorInsertPrix = "Ce prix est ivalide,Veuillez saisir à nouveau \
+\nEt verifier bien quand il s'agit de chiffre!!!"
+
+gallerry = "Voici donc les galleries de ce produit\n\nVous pouvez \
+les supprimmer ou ajouter à nouveau!!"
+
+supprimmer = "Supprimé avec succées"
+
+erreurNbGallerryModifier = "Votre nombre des photos depasse le nombre à inserer \
+\n\nVeuillez-vous envoyer à nouveau en respectant le nombre à inserer"
+
+attenteGallerry = "Entrer ensemble les photos de gallery du ce produit \
+\n\nEt vous pouvez envoyez jusq'à 10 photos pour la galerie"
+
+listData = "Listes de données"
+inputProductName = "Entrer le nom du produit à créer"
+inputDetail = "Entrer l'image de details du produit"
+inputPrix = "Entrer le prix par heure de ce produit"
+inputPdc = "Entrer la photo de couverture du ce produit"
+incorrectPrix = "Prix incorrect!\nLe prix est forcement des chiffre \
+\n\nAlors veuillez saisir à nouveau"
+
+inputNewName = "Entrer le nouveau nom:"
+inputnewDetails = "Entrer la photo du nouveau details:"
+inputnewPrix = "Entrer le nouveau Prix:"
+inputNewPdc = "Entrer la nouvelle photo de couverture :"
+
+ErrorTypeDetails = "Votre photo de Details est invalide\n\n \
+Envoyez plutôt du photo"
+
+ErrorTypePdc = "Votre photo de Couverture est invalide\n\n \
+Envoyez plutôt du photo"
+
+ErrorTypeGallery = "Vos photos pour les Galeries sont invalide\n\n \
+Envoyez plutôt du photo"
+
+successAddProduct = " Produit ajouté avec succès"
+successDelete = "Supprimé avec SUCCES"
+
+ErrorInputImageUser = "Evoyez plutôt du text pour continuer 😊😊😊"
+

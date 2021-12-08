2048b454442ad0a822c57337cd8fc4c9148039bc
SergioDev22 <louis.sergio.rakotoarisoa@esti.mg>
2021-12-08 05:41:48 -0600

    Le corp de scène specifiquement aux partenaires

---
+++ partenaire.py
@@ -0,0 +1,13 @@
+import messenger
+from conf import ACCESS_TOKEN,URL_SERVER
+import requete
+import const
+import json
+
+bot = messenger.Messenger(ACCESS_TOKEN)
+reqPart= requete.Requete()
+
+class Partenaire:
+    def __init__(self):
+        pass
+

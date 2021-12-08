28e2630a4971c2922d90630025e33e39ec0dc4c9
SergioDev22 <louis.sergio.rakotoarisoa@esti.mg>
2021-12-08 05:39:01 -0600

    Fichier d'anticipation des erreurs

---
+++ erreur.py
@@ -0,0 +1,11 @@
+'''
+    Objet pour les simulations d'erreur
+'''
+
+
+class TestUserRequests:
+    '''
+        Objet simulant un non envoi d'une requete
+    '''
+    status_code = 200
+    text = "Pas d'envoi pour un utilisateur test"
